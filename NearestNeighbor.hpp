08730e27f969384a799cf27e2c9c8773f9638d00
danielkaijzer <danielkaijzer00@gmail.com>
2023-12-10 20:09:50 -0500

    Project NN v1 done

--- NearestNeighbor.hpp
+++ NearestNeighbor.hpp
@@ -1,10 +1,95 @@
 #ifndef NEAREST_NEIGHBOR_HPP
 #define NEAREST_NEIGHBOR_HPP
 
-#include "Node.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <chrono>
 
-class NearestNeigbor{
+class Node {
+public:
+    int id; // unique id for city
+    double x, y; // coordinates
 
+    Node(int _id, double _x, double _y) : id(_id), x(_x), y(_y) {}
+
+    double distance(const Node& other) const {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return std::sqrt(dx * dx + dy * dy); // euclidean distance
+    }
+};
+
+
+class NearestNeighbor {
+private:
+    std::list<Node> unvisitedNodes;
+    std::list<Node> tour;
+    double totalDistance;
+
+public:
+   NearestNeighbor(const std::vector<Node>& nodes) : unvisitedNodes(nodes.begin(), nodes.end()), totalDistance(0.0) {}
+
+    // helper function for run()
+    std::list<Node>::iterator findNearest(const Node& current)
+    {
+        auto nearest = unvisitedNodes.begin(); // 
+        double minDistance = current.distance(*nearest);
+
+        for (auto it = unvisitedNodes.begin(); it != unvisitedNodes.end(); ++it) {
+            double dist = current.distance(*it);
+            // if you find closer city, choose that city instead
+            if (dist < minDistance) {
+                minDistance = dist;
+                nearest = it;
+            }
+        }
+
+        // return closest unvisited city
+        return nearest;
+    }
+
+    // print outputs
+        // id's in order visited
+        // total distance
+        // time in milliseconds
+    void printResult(long long duration) const {
+        for (const auto& node : tour) {
+            std::cout << node.id << " ";
+        }
+
+        std::cout << "\nTotal Distance: " << totalDistance << "\n";
+        std::cout << "Time in ms: " << duration << "\n";
+    }
+
+    // main function
+    void run() {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        // Start with node 1
+        Node current = unvisitedNodes.front();
+        tour.push_back(current);
+        unvisitedNodes.pop_front();
+
+        // Visit the nearest neighbor until all nodes are visited
+        while (!unvisitedNodes.empty()) {
+            auto nearest = findNearest(current);
+            totalDistance += current.distance(*nearest);
+            tour.push_back(*nearest);
+            current = *nearest;
+            unvisitedNodes.erase(nearest);
+        }
+
+        // Return to node 1 to complete tour/cycle
+        totalDistance += current.distance(tour.front());
+        tour.push_back(tour.front());
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        printResult(duration);
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,73 @@
-// Testing file
-
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
 #include "NearestNeighbor.hpp"
 
-int main(){
+// Function to trim whitespaces from the beginning and end of a string
+std::string trim(const std::string& str) {
+    size_t first = str.find_first_not_of(" \t\n\r\f\v");
+    size_t last = str.find_last_not_of(" \t\n\r\f\v");
+    return (first != std::string::npos && last != std::string::npos) ? str.substr(first, last - first + 1) : "";
+}
+
+int main() {
+    // Read input data from file (replace "wi29.tsp" with the actual file name)
+    std::ifstream input("ja9847.tsp");
+
+    if (!input.is_open()) {
+        std::cerr << "Error opening file.\n";
+        return 1;
+    }
+
+    std::vector<Node> nodes;
+
+    std::string line;
+    bool readingNodes = false;
+
+    while (std::getline(input, line)) {
+        // Trim whitespaces from the line
+        line = trim(line);
+
+        if (line == "NODE_COORD_SECTION") {
+            // std::cout << "found ncs\n";
+            readingNodes = true;
+            continue;
+        }
+
+        if (readingNodes && line == "EOF") {
+            // std::cout << line << std::endl;
+            break;  // Stop reading after reaching "EOF"
+        }
+
+        if (readingNodes && !line.empty()) {
+            // std::cout << "hello\n";
+            // Parse node data
+            int id;
+            double x, y;
+            std::istringstream iss(line);
+            iss >> id >> x >> y;
+
+            if (iss.fail()) {
+                std::cerr << "Error parsing line: " << line << "\n";
+                return 1;
+            }
+
+            nodes.emplace_back(id, x, y);
+        }
+    }
+
+    // for (auto n : nodes) {
+    //     std::cout << n.id << std::endl;
+    // }
+
+    if (!nodes.empty()) {
+        NearestNeighbor nn(nodes);
+        nn.run();
+    } else {
+        std::cout << "Didn't read data correctly\n";
+        return -1;
+    }
+
     return 0;
 }
