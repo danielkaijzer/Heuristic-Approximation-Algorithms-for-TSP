e141ddafc00fbed8579b29605d1206e9ed37ddb5
Daniel <69543129+danielkaijzer@users.noreply.github.com>
2024-09-08 14:49:03 -0400

    Update main.cpp
    Uncommented a few lines and cleaned up

--- main.cpp
+++ main.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
-// #include "NearestNeighbor.hpp"
+#include "NearestNeighbor.hpp"
 #include "GreedyTSP.hpp"
 
 
 
 int main() {
-    greedyTSP("wi29.tsp"); // 30,052
+
+    nearestNeighbor("wi29.tsp"); 
     std::cout << std::endl;
 
-    // greedyTSP("dj38.tsp"); // 7019, time 1ms
-    // std::cout << std::endl;
+    greedyTSP("wi29.tsp"); 
+    std::cout << std::endl;
 
-    // greedyTSP("qa194.tsp"); // 11,521, time: 29ms
-    // std::cout << std::endl;
+    nearestNeighbor("dj38.tsp"); 
+    std::cout << std::endl;
 
-    // nearestNeighbor("qa194.tsp");
-    // std::cout << std::endl;
+    greedyTSP("dj38.tsp"); 
+    std::cout << std::endl;
 
-    // nearestNeighbor("zi929.tsp");
-    // std::cout << std::endl;
+    nearestNeighbor("qa194.tsp");
+    std::cout << std::endl;
 
-    // greedyTSP("zi929.tsp"); // 102,456, time 414ms
-    // std::cout << std::endl;
+    greedyTSP("qa194.tsp"); 
+    std::cout << std::endl;
 
-    // greedyTSP("ja9847.tsp"); // 557,018, time 73818ms
-    // std::cout << std::endl;
+    nearestNeighbor("zi929.tsp");
+    std::cout << std::endl;
 
-    // nearestNeighbor("ja9847.tsp");
-    // std::cout << std::endl;
+    greedyTSP("zi929.tsp"); 
+    std::cout << std::endl;
 
-    // nearestNeighbor("");
+    nearestNeighbor("ja9847.tsp");
+    std::cout << std::endl;
+
+    greedyTSP("ja9847.tsp"); 
+    std::cout << std::endl;
 
     return 0;
 }
